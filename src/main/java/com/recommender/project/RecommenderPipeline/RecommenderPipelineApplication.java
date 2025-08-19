94f30f3af1a96a16c3d13aa739e5d424d33ba305
hoangviet-nguyen <viety.nguyen02@gmail.com>
2025-08-19 09:10:18 +0200

    initial commit

---
+++ src/main/java/com/recommender/project/RecommenderPipeline/RecommenderPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.recommender.project.RecommenderPipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommenderPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommenderPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recommender/project/RecommenderPipeline/RecommenderPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recommender.project.RecommenderPipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommenderPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
