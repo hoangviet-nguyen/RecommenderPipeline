633db319b57664f2d6218c990f995926ab0215cb
hoangviet-nguyen <viety.nguyen02@gmail.com>
2025-08-26 20:31:09 +0200

    Changed UserRate to APIRating

--- src/main/java/com/recommender/project/RecommenderPipeline/data_models/wrapper/APIRating.java
+++ src/main/java/com/recommender/project/RecommenderPipeline/data_models/wrapper/APIRating.java
@@ -1,5 +1,6 @@
 package com.recommender.project.RecommenderPipeline.data_models.wrapper;
 
+import com.google.gson.annotations.SerializedName;
 import com.recommender.project.RecommenderPipeline.data_models.Anime;
 
-public record UserRate(String id, Anime anime, int score) {}
+public record APIRating(@SerializedName("id") String userId, Anime anime, int score) {}
