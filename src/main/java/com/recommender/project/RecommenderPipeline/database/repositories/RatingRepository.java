4dea7820dea786677368a3b9f690f728162ed84f
hoangviet-nguyen <viety.nguyen02@gmail.com>
2025-09-05 17:24:15 +0200

    added findByAnimeIdAndUserId method

--- src/main/java/com/recommender/project/RecommenderPipeline/database/repositories/RatingRepository.java
+++ src/main/java/com/recommender/project/RecommenderPipeline/database/repositories/RatingRepository.java
@@ -4,11 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
-
 import com.recommender.project.RecommenderPipeline.data_models.Rating;
 
 public interface RatingRepository extends CrudRepository <Rating, Long>{
     Optional<Rating> findById(Long id);
     List<Rating> findByUserId(Long userId);
-    List<Rating> findByAnimeId(Long animeId);
+    List<Rating> findByAnimeId(String animeId);
+    List<Rating> findByAnimeDbId(Long animeDbId);
+    Optional<Rating> findByAnimeIdAndUserId(String animeId, Long userId);
 }
