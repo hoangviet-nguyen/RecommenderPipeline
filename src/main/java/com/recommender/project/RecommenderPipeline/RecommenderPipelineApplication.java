dcbe8ed82343f53d77decbaaee8a80afd1e7b8e6
hoangviet-nguyen <viety.nguyen02@gmail.com>
2025-09-05 17:25:18 +0200

    Formatting

--- src/main/java/com/recommender/project/RecommenderPipeline/RecommenderPipelineApplication.java
+++ src/main/java/com/recommender/project/RecommenderPipeline/RecommenderPipelineApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RecommenderPipelineApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RecommenderPipelineApplication.class, args);
 	}
